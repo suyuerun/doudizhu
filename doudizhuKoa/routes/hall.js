9d60fdb8948016be6151f4f9e574db814b0cead2
suyuerun <972765730@qq.com>
2017-06-07 19:21:14 +0800

    v1.2

--- doudizhuKoa/routes/hall.js
+++ doudizhuKoa/routes/hall.js
@@ -10,4 +10,15 @@ router.get('/getRoomList', async function (ctx, next) {
   }
   ctx.body=[];
 })
+
+router.get('/newroom', async function (ctx, next) {
+  let loginbean = ctx.session.loginbean;
+  if(!loginbean){
+  	ctx.body='登陆过期';
+  	return;
+  }
+  let roompwd = ctx.qurey.roompwd;
+})
+
+
 module.exports = router;
